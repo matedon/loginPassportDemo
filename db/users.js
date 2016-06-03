2e2250de8bb24d4a976546c7debaa4cda73b87b1
kotroczo.mate <kotroczo.mate@virgo.hu>
2016-06-03 11:03:00 +0200

    remove non-used ui items, read user with promise fs json

--- db/users.js
+++ db/users.js
@@ -1,40 +1,69 @@
-var records = [
-    {id: 1, username: 'jack', password: 'secret', displayName: 'Jack', email: 'jack@example.com'},
-    {id: 2, username: 'jill', password: 'birthday', displayName: 'Jill', email: 'jill@example.com'}
-];
+const
+    Promise = require("bluebird"),
+    fs = require('fs'),
+    path = require('path');
+
+function loadUserRecors () {
+    return new Promise(function (fulfill, reject) {
+        var filePath = path.join(__dirname, 'users.json');
+        fs.access(filePath, fs.R_OK, function (err) {
+            var records = [];
+            if (err) {
+                console.error('error', err);
+                reject(err);
+            } else {
+                var data = fs.readFileSync(filePath);
+                records = JSON.parse(data);
+                console.log('records', records);
+            }
+            fulfill(records);
+        });
+    });
+};
 
 exports.findById = function (id, cb) {
-    process.nextTick(function () {
-        var idx = id - 1;
-        if (records[idx]) {
-            cb(null, records[idx]);
-        } else {
-            cb(new Error('User ' + id + ' does not exist'));
-        }
-    });
+    loadUserRecors()
+        .then(function (records) {
+            process.nextTick(function () {
+                var idx = id - 1;
+                if (records[idx]) {
+                    cb(null, records[idx]);
+                } else {
+                    cb(new Error('User ' + id + ' does not exist'));
+                }
+            });
+        })
+    ;
 };
 
 exports.findByEmail = function (email, cb) {
-    console.log(arguments);
-    process.nextTick(function () {
-        for (var i = 0, len = records.length; i < len; i++) {
-            var record = records[i];
-            if (record.email === email) {
-                return cb(null, record);
-            }
-        }
-        return cb(null, null);
-    });
+    loadUserRecors()
+        .then(function (records) {
+            process.nextTick(function () {
+                for (var i = 0, len = records.length; i < len; i++) {
+                    var record = records[i];
+                    if (record.email === email) {
+                        return cb(null, record);
+                    }
+                }
+                return cb(null, null);
+            });
+        })
+    ;
 };
 
 exports.findByUsername = function (username, cb) {
-    process.nextTick(function () {
-        for (var i = 0, len = records.length; i < len; i++) {
-            var record = records[i];
-            if (record.username === username) {
-                return cb(null, record);
-            }
-        }
-        return cb(null, null);
-    });
+    loadUserRecors()
+        .then(function (records) {
+            process.nextTick(function () {
+                for (var i = 0, len = records.length; i < len; i++) {
+                    var record = records[i];
+                    if (record.username === username) {
+                        return cb(null, record);
+                    }
+                }
+                return cb(null, null);
+            });
+        })
+    ;
 };
